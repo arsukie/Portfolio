24529ab09ee3707c665edadf89bce5726c27afce
Arjun Khanal <np.arkhanal@gmail.com>
2026-08-24 17:09:04 +0545

    Hackatime stats and warning addition with few assets update

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ buttons.forEach(button => {
         this.classList.add('active');
     });
 });
+
+function updateClock() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    document.getElementById("clock").textContent =
+        `${hours}:${minutes}:${seconds}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
